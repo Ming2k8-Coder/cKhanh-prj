a470ad0186e4c58ec353c2c59ab055af1ac19694
Ming2k8-Coder <nguyentuanminh208@gmail.com>
2023-07-14 08:43:16 +0700

    modified:   RandomGenNum.cpp

--- RandomGenNum.cpp
+++ RandomGenNum.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
+    int a;
+    cout<<"Random Number Generator(RNG)"<<endl;
+    cout<<"Số tối đa: ";
+    cin>>a;
     srand((unsigned) time(NULL));
-    int random = rand() % 10; //Range
-    cout<<random; 
+    int random = rand() % a; //Range
+    cout<<"Số đã tạo: "<<random; 
 }
