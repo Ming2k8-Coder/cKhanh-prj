e02f65c36bfab1f71d0301267594d993dd50fb11
Ming2k8-Coder <nguyentuanminh208@gmail.com>
2023-07-14 11:48:56 +0700

    modified:   c11.cpp 	modified:   c11.exe

--- c11.cpp
+++ c11.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"BT Câu 11";
     cout<<"Nhập số bé  hơn 10^7 và in ra dãy từ 0:";
     cin>> num;
-    if (num>10^7){
+    if (num>10000000){
         cout<<"Số đã lớn hơn 10^7,có khả năng tràn console,mời nhập lại";
         cin>> num;}
     cout<<"Dãy số bắt đầu:";
